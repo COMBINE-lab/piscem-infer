e289ffa0324bffbe4b8d77d1c635e928de9bc294
rob-p <rob@cs.umd.edu>
2025-07-18 22:22:23 -0400

    fully parameterized by eq class

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod process_rad;
 mod prog_opts;
 mod utils;
 use prog_opts::{Cli, Commands};
+use utils::eq_maps::EqMapType;
 
 use crate::process_rad::process_bulk;
 
@@ -19,7 +20,14 @@ fn main() -> anyhow::Result<()> {
     }
 
     match cli_args.command {
-        Commands::Quant(quant_opts) => process_bulk(quant_opts)?,
+        Commands::Quant(quant_opts) => {
+            let eqc_type = if quant_opts.factorized_eqc_bins > 1 {
+                EqMapType::RangeFactorizedEqMap
+            } else {
+                EqMapType::BasicEqMap
+            };
+            process_bulk(quant_opts, eqc_type)?
+        }
     }
     Ok(())
 }
--- src/prog_opts.rs
+++ src/prog_opts.rs
@@ -1,5 +1,6 @@
 use clap::Args;
 use clap::{Parser, Subcommand};
+use clap_num::number_range;
 
 use serde::Serialize;
 use std::path::PathBuf;
@@ -9,6 +10,10 @@ use crate::utils::map_record_types::LibraryType;
 const PRESENCE_THRESH: f64 = 1e-8;
 const RELDIFF_THRESH: f64 = 1e-3;
 const MAX_EM_ITER: u32 = 1500;
+
+fn greater_than_0(s: &str) -> std::result::Result<u32, String> {
+    number_range(s, 1, u32::MAX)
+}
 
 #[derive(Args, Serialize, Clone, Debug)]
 pub struct QuantOpts {
@@ -35,6 +40,10 @@ pub struct QuantOpts {
     /// to be read from the file and interrogated twice.
     #[arg(long, default_value_t = 500_000_isize)]
     pub param_est_frags: isize,
+    /// number of probability bins to use in RangeFactorized equivalence classes.
+    /// If this value is set to 1, then basic equivalence classes are used.
+    #[arg(long, default_value_t = 64_u32, value_parser=greater_than_0)]
+    pub factorized_eqc_bins: u32,
     /// mean of fragment length distribution mean
     /// (required, and used, only in the case of unpaired fragments).
     #[arg(long, requires = "fld_sd")]
